3b61fbe50a6fcc379f88bf09d812a902cc074dc1
Felipemv <felipemartinsv97@gmail.com>
2018-10-31 02:42:26 -0300

    Adicionado teste para classe principal.

---
+++ src/test/java/br/inatel/ec206/testedecobertura/TesteDeCoberturaTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.inatel.ec206.testedecobertura;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuário
+ */
+public class TesteDeCoberturaTest {
+    
+    public TesteDeCoberturaTest() {
+    }
+
+    /**
+     * Test of main method, of class TesteDeCobertura.
+     */
+    @Test
+    public void testMain() {
+    }
+    
+}
